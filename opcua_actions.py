f77d11590046aff5a383a46726c0539031466838
Grzesiek <84570140+grzesiek2201@users.noreply.github.com>
2022-03-01 18:14:55 +0100

    Update opcua_actions.py

--- opcua_actions.py
+++ opcua_actions.py
@@ -1,4 +1,5 @@
 from opcua import Client
+from opcua import ua
 import time
 
 
@@ -17,13 +18,12 @@ class OpcuaClient:
     def add_data(self):
         """ Appends data to the self.data attribute """
         self.data.append(("[size=14]16.02.2022", "[size=14]10", "[size=14]50", "[size=14]3", "[size=14]4", "[size=14]36", "[size=14]12", "[size=14]#F"))
-        #self.data.append(tuple(self.read_data()))  # it's very probable that there will be need to adjust the data format
 
     def read_data(self, ns=4, i=6):
         """ reads data from client's node defined in the function and returns all the values as a list """
         node = self.client.get_node(f'ns={ns};i={i}')
         row = []
-        for child in node.get_children(): # w jakiej kolejności są te dane? według indeksów i czy według kolejności jak w bloku danych?
+        for child in node.get_children():  # data is read in the order specified in the datablock
             row.append(child.get_value())
         row = [f"[size=14]{value}" for value in row]
         print(row)
@@ -32,7 +32,19 @@ class OpcuaClient:
     def send_data(self, value, ns=4, i=4):
         """ Sends data to specified ns and i """
         variable = self.client.get_node(f'ns={ns};i={i}')
-        variable.set_value(value)
+        dv = None
+        if type(value) == float:
+            dv = ua.DataValue(ua.Variant(value, ua.VariantType.Float))
+        elif type(value) == bool:
+            dv = ua.DataValue(ua.Variant(value, ua.VariantType.Boolean))
+        elif type(value) == int:
+            dv = ua.DataValue(ua.Variant(value, ua.VariantType.Int16))
+        if dv:
+            dv.ServerTimestamp = None
+            dv.SourceTimestamp = None
+            variable.set_value(dv)
+        else:
+            raise AttributeError
 
     def terminate_client(self):
         self.client.disconnect()
